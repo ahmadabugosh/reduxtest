8efc0e5819ad3a08925d056b46b28fe3bc151bbb
Ahmad Abugosh <ahmadabugosh@gmail.com>
2018-01-27 00:09:26 +0400

    Added simple redux reducer and action

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import {createStore} from 'redux';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -18,4 +20,29 @@ class App extends Component {
   }
 }
 
+
 export default App;
+
+const defaultState ={
+
+  welcome:'Hi',
+  otherstate:'some state'
+}
+
+const greeting =(state=defaultState, action)=>{
+  switch(action.type)
+  {
+    case 'GREET_ME':
+      return{...state, welcome:'Hello there'}
+    case 'GREET_WORLD':
+      return {...state,welcome:'Hello world'}
+    default:
+      return state;
+  }
+
+
+};
+const store=createStore(greeting);
+
+store.dispatch({type:'GREET_ME'})
+console.log(store.getState());
